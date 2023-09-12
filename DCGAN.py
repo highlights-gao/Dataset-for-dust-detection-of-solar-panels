f5a4a0821a1ad13a61d8f9c93dea062914acc587
Highlights_gao <gao.jianguang9@gmail.com>
2023-09-12 21:56:54 +0200

    datasets changes

--- DCGAN.py
+++ DCGAN.py
@@ -15,53 +15,59 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-# Set random seed for reproducibility
-manualSeed = 999
-#manualSeed = random.randint(1, 10000) # use if new results are needed.
-#print("Random Seed: ", manualSeed)
-random.seed(manualSeed)
-torch.manual_seed(manualSeed)
-torch.use_deterministic_algorithms(True) # Needed for reproducible results
+def main():
+    # Set random seed for reproducibility
+    # Set random seed for reproducibility
+    manualSeed = 999
+    #manualSeed = random.randint(1, 10000) # use if new results are needed.
+    #print("Random Seed: ", manualSeed)
+    random.seed(manualSeed)
+    torch.manual_seed(manualSeed)
+    torch.use_deterministic_algorithms(True) # Needed for reproducible results
 
-# set parameters
-dataroot = 'data\img_align_celeba'
-# number of workers for dataloader
-workers = 2
+    # set parameters
+    dataroot = 'datasets\Solar Panel Images Clean and Faulty Images'
+    # number of workers for dataloader
+    workers = 2
 
-batch_size = 128
-image_size = 64
-# number of channels in the training images. 
-nc =3 
-# size of  z latent vector(size of generator input)
-nz = 100
-#size of feature maps in generator
-ngf = 64
-#size of feature maps in discriminator
-ndf = 64
-#number of training epochs 
-num_epochs = 5
-#number of GPUs available. Use 0 for CPU mode.
-ngpu = 1
+    batch_size = 128
+    image_size = 64
+    # number of channels in the training images. 
+    nc =3 
+    # size of  z latent vector(size of generator input)
+    nz = 100
+    #size of feature maps in generator
+    ngf = 64
+    #size of feature maps in discriminator
+    ndf = 64
+    #number of training epochs 
+    num_epochs = 5
+    #number of GPUs available. Use 0 for CPU mode.
+    ngpu = 1
 
-# We can use an image folder dataset the way we have it setup.
-# Create the dataset
-dataset = dset.ImageFolder(root=dataroot,
-                           transform=transforms.Compose([
-                               transforms.Resize(image_size),
-                               transforms.CenterCrop(image_size),
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                           ]))
-# Create the dataloader
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                         shuffle=True, num_workers=workers)
+    # We can use an image folder dataset the way we have it setup.
+    # Create the dataset
+    dataset = dset.ImageFolder(root=dataroot,
+                            transform=transforms.Compose([
+                                transforms.Resize(image_size),
+                                transforms.CenterCrop(image_size),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                            ]))
+    # Create the dataloader
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                            shuffle=True, num_workers=workers)
 
-# Decide which device we want to run on
-device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+    # Decide which device we want to run on
+    device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+    print(device)
+    # Plot some training images
+    real_batch = next(iter(dataloader))
+    plt.figure(figsize=(8,8))
+    plt.axis("off")
+    plt.title("Training Images")
+    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+    
 
-# Plot some training images
-real_batch = next(iter(dataloader))
-plt.figure(figsize=(8,8))
-plt.axis("off")
-plt.title("Training Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+if __name__ == '__main__' :
+    main()
